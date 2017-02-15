cf695a5072cd2595b30d05e6eb3946f3aceae669
neeil1990 <neeil@mail.ru>
2017-02-15 14:34:27 +0300

    no message

---
+++ mail-test.php
@@ -0,0 +1,9 @@
+<?
+
+$message = "Line 1\r\nLine 2\r\nLine 3";
+
+// На случай если какая-то строка письма длиннее 70 символов мы используем wordwrap()
+$message = wordwrap($message, 70, "\r\n");
+
+// Отправляем
+mail('kovalev.neil@yandex.ru', 'My Subject', $message);
