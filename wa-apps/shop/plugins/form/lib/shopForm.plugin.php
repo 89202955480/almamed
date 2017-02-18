9d45d48265b6ae57c9169bd7a0ac40f1cad0eb3d
neeil1990 <neeil@mail.ru>
2017-02-18 18:57:36 +0300

    no message

--- wa-apps/shop/plugins/form/lib/shopForm.plugin.php
+++ wa-apps/shop/plugins/form/lib/shopForm.plugin.php
@@ -67,7 +67,7 @@ class shopFormPlugin extends shopPlugin
 
             $headers  = 'MIME-Version: 1.0' . "\r\n";
             $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
-            
+
             $send = mail($to, $subject, $message, $headers);
 
             ?>
