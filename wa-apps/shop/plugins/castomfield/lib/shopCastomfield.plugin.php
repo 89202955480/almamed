eda588879c3a1456bca92f7ff4dbe2d3191d9e11
neeil1990 <neeil@mail.ru>
2018-03-29 17:49:09 +0300

    no message

--- wa-apps/shop/plugins/castomfield/lib/shopCastomfield.plugin.php
+++ wa-apps/shop/plugins/castomfield/lib/shopCastomfield.plugin.php
@@ -17,7 +17,20 @@ class shopCastomfieldPlugin extends shopPlugin
                 <div class="value">
                     <textarea name="product[comment_user]">'.$param->comment_user.'</textarea>
                 </div>
-            </div>',
+            </div>
+            <div class="field">
+                <div class="name">Цена</div>
+                <div class="value">
+                    <input type="text" name="product[price_user]" value="'.$param->price_user.'">
+                </div>
+            </div>
+            <div class="field">
+                <div class="name">Срок поставки</div>
+                <div class="value">
+                    <input type="text" name="product[delivery_user]" value="'.$param->delivery_user.'">
+                </div>
+            </div>
+            ',
         );
     }
 
@@ -31,7 +44,18 @@ class shopCastomfieldPlugin extends shopPlugin
             $html = $view->fetch($_SERVER['DOCUMENT_ROOT'].self::getStaticUrlPlugin().'templates/actions/Frontend.html');
             return $html;
         }
+    }
 
+    public function access_user_cart($param){
+        $plugin = wa('shop')->getPlugin('castomfield');
+        $settings = $plugin->getSettings();
+        if($settings){
+            $view = wa()->getView();
+            $view->assign('settings', explode(",", $settings['access_user']));
+            $view->assign('param', $param);
+            $html = $view->fetch($_SERVER['DOCUMENT_ROOT'].self::getStaticUrlPlugin().'templates/actions/FrontendCart.html');
+            return $html;
+        }
     }
 }
 
